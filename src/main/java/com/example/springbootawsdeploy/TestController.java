dde7e8c12aede97ba329b2a67ae30ef2ada64cb4
hinakeshar1234 <hinakesharwani92@gmail.com>
2026-02-18 15:41:46 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -1,12 +1,14 @@
 package com.example.springbootawsdeploy;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller   // Change from @RestController to @Controller for HTML
 @RequestMapping("/demo")
 public class TestController {
+
     @GetMapping("/data")
     public String getData() {
         return "First message from AWS Ecs";
@@ -15,5 +17,10 @@ public class TestController {
     @GetMapping("/message")
     public String getMessage() {
         return "Second message from AWS Ecs";
+    }
+
+    @GetMapping("/")   // <-- Add this for root
+    public String home() {
+        return "index";   // templates/index.html serve karega
     }
 }
